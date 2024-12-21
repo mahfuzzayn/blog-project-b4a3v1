caf14300c64ccceaf27fb6d5da03fb420c43d902
mahfuzzayn <mahfuzzayn8@gmail.com>
2024-12-21 11:57:27 +0600

    Made Global Error Handler & Type Based Error Handler as Zod, Validation, Duplicate etc.

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,28 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import router from './app/routes'
+import notFound from './app/middlewares/notFound'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
 const app: Application = express()
 
-// parser
+// Parsers
 app.use(express.json())
 app.use(cors())
 
 // Application Routes
 app.use('/api', router)
 
-// homepage routes
+// Homepage Route
 app.get('/', (req: Request, res: Response) => {
     res.send({
         message: 'Welcome to the Blog API',
     })
 })
+
+// Global Error Handler
+app.use(globalErrorHandler)
+
+// Not Found
+app.use(notFound)
 
 export default app
