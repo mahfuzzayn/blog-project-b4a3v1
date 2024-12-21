c9975a624449026daa7b6edfbd354893bb2b0536
mahfuzzayn <mahfuzzayn8@gmail.com>
2024-12-21 21:01:47 +0600

    Added Internal Server Error Handling Features

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose'
-import config from './app/config'
 import app from './app'
+import config from './app/config'
+import mongoose from 'mongoose'
+import { Server } from 'http'
+
+let server: Server
 
 async function main() {
     try {
         await mongoose.connect(config.database_url as string)
-        app.listen(config.port, () => {
-            console.log(
-                `Server is listening on http://localhost:${config.port}`,
-            )
+        server = app.listen(config, () => {
+            console.log(`App listening on port ${config.port} with database status: running`)
         })
     } catch (error) {
         console.log(error)
@@ -16,3 +17,17 @@ async function main() {
 }
 
 main()
+
+process.on('unhandledRejection', error => {
+    console.log('⚠️ unhandledRejection is detected, server is shutting down ...', error)
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+})
+
+process.on('uncaughtException', () => {
+    console.log('⚠️ uncaughtException is detected, server is shutting down ...')
+    process.exit(1)
+})
