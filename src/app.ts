8bed0432dfa62c9210a758bc36fa4880faac01ce
mahfuzzayn <mahfuzzayn8@gmail.com>
2024-12-21 10:27:30 +0600

    Implemented User, Auth Modules with models, interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import router from './app/routes'
 const app: Application = express()
 
 // parser
 app.use(express.json())
 app.use(cors())
+
+// Application Routes
+app.use('/api', router)
 
 // homepage routes
 app.get('/', (req: Request, res: Response) => {
